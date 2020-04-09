fb71b7b231d8a76a06d7970f3bfcd9311f094c8b
feroliv4z <feroliv4z@gmail.com>
2020-04-08 23:26:18 -0600

    arrancando a armar el arbol de expresiones regulares

--- PROYECTO_LFA_1251518/PROYECTO_LFA_1251518/TreeGenerator.cs
+++ PROYECTO_LFA_1251518/PROYECTO_LFA_1251518/TreeGenerator.cs
@@ -11,9 +11,13 @@ namespace PROYECTO_LFA_1251518
         private Queue<BynaryTree<Node>> expresionQueue;
         private int simbolCount;
         private List<Simbol> simbolList;
+        private BynaryTree<Node> expresionTree;
         public void generate(string regex)
         {
             this.buildQueue(regex);
+            this.expresionTree = new BynaryTree<Node>(new Node("."));
+            this.expTree.Left = (IBinaryTree<Node>)this.generateExpression(ref this.expresionQueue);
+            this.expTree.Right = (IBinaryTree<Node>)this.expresionQueue.Dequeue();
         }
         private void buildQueue(string regex)
         {
@@ -113,6 +117,196 @@ namespace PROYECTO_LFA_1251518
                     --i;
                 }
             }
+        }
+        public BynaryTree<Node> expTree
+        {
+            get
+            {
+                return this.expresionTree;
+            }
+        }
+        private BynaryTree<Node> generateExpression(ref Queue<BynaryTree<Node>> expresionQueue)
+        {
+            Stack<BynaryTree<Node>> bynaryTrees = new Stack<BynaryTree<Node>>();
+            if (expresionQueue == null)
+                throw new Exception("No existe expresión regular para evaluar.");
+            while (expresionQueue.Count > 0)
+            {
+                if (expresionQueue.Peek().Value.Simbol.Equals("#"))
+                {
+                    if (bynaryTrees.Count <= 1)
+                        return bynaryTrees.Pop();
+                    return new BynaryTree<Node>(new Node("."))
+                    {
+                        Right = (IBinaryTree<Node>)bynaryTrees.Pop(),
+                        Left = (IBinaryTree<Node>)bynaryTrees.Pop()
+                    };
+                }
+                BynaryTree<Node> bynaryTree1 = expresionQueue.Dequeue();
+                if (this.operatorSimbol(bynaryTree1))
+                {
+                    bynaryTrees.Push(bynaryTree1);
+                    if (bynaryTrees.Count >= 3)
+                    {
+                        BynaryTree<Node> binaryTree2 = bynaryTrees.Pop();
+                        BynaryTree<Node> binaryTree3 = bynaryTrees.Pop();
+                        BynaryTree<Node> binaryTree4 = bynaryTrees.Pop();
+                        bynaryTrees.Push(new BynaryTree<Node>(new Node("."))
+                        {
+                            Right = (IBinaryTree<Node>)binaryTree3,
+                            Left = (IBinaryTree<Node>)binaryTree4
+                        });
+                        bynaryTrees.Push(binaryTree2);
+                    }
+                }
+                else
+                {
+                    if (!this.isOperator(bynaryTree1))
+                        throw new Exception("Error en la expresion regular");
+                    if ("+*?".Contains(bynaryTree1.Value.Simbol))
+                    {
+                        bynaryTree1.Left = (IBinaryTree<Node>)bynaryTrees.Pop();
+                        bynaryTrees.Push(bynaryTree1);
+                    }
+                    else if ("(".Equals(bynaryTree1.Value.Simbol))
+                    {
+                        bynaryTrees.Push(this.generateExpression(ref expresionQueue));
+                        if (bynaryTrees.Count >= 3)
+                        {
+                            BynaryTree<Node> arbolBinarioBase2 = bynaryTrees.Pop();
+                            BynaryTree<Node> arbolBinarioBase3 = bynaryTrees.Pop();
+                            BynaryTree<Node> arbolBinarioBase4 = bynaryTrees.Pop();
+                            bynaryTrees.Push(new BynaryTree<Node>(new Node("."))
+                            {
+                                Right = (IBinaryTree<Node>)arbolBinarioBase3,
+                                Left = (IBinaryTree<Node>)arbolBinarioBase4
+                            });
+                            bynaryTrees.Push(arbolBinarioBase2);
+                        }
+                    }
+                    else
+                    {
+                        if (")".Equals(bynaryTree1.Value.Simbol))
+                        {
+                            if (bynaryTrees.Count <= 1)
+                                return bynaryTrees.Pop();
+                            return new BynaryTree<Node>(new Node("."))
+                            {
+                                Right = (IBinaryTree<Node>)bynaryTrees.Pop(),
+                                Left = (IBinaryTree<Node>)bynaryTrees.Pop()
+                            };
+                        }
+                        if ("|".Equals(bynaryTree1.Value.Simbol))
+                        {
+                            if (bynaryTrees.Count <= 1)
+                            {
+                                bynaryTree1.Left = (IBinaryTree<Node>)bynaryTrees.Pop();
+                                bynaryTree1.Right = (IBinaryTree<Node>)this.generateOr(ref expresionQueue);
+                                bynaryTrees.Push(bynaryTree1);
+                            }
+                            else
+                            {
+                                bynaryTree1.Left = (IBinaryTree<Node>)new BynaryTree<Node>(new Node("."))
+                                {
+                                    Right = (IBinaryTree<Node>)bynaryTrees.Pop(),
+                                    Left = (IBinaryTree<Node>)bynaryTrees.Pop()
+                                };
+                                bynaryTree1.Right = (IBinaryTree<Node>)this.generateOr(ref expresionQueue);
+                                bynaryTrees.Push(bynaryTree1);
+                            }
+                        }
+                    }
+                }
+            }
+            return bynaryTrees.Pop();
+        }
+        private bool operatorSimbol(BynaryTree<Node> node)
+        {
+            return !"()*+?.|".Contains(node.Value.Simbol);
+        }
+
+        private bool isOperator(BynaryTree<Node> node)
+        {
+            return !this.operatorSimbol(node);
+        }
+        private BynaryTree<Node> generateOr(ref Queue<BynaryTree<Node>> expressionQueue)
+        {
+            Stack<BynaryTree<Node>> arbolBinarioBaseStack = new Stack<BynaryTree<Node>>();
+            if (expressionQueue == null)
+                throw new Exception("Error en la expresion regular");
+            while (expressionQueue.Count > 0)
+            {
+                BynaryTree<Node> arbolBinarioBase1 = expressionQueue.Peek();
+                if (this.operatorSimbol(arbolBinarioBase1))
+                {
+                    BynaryTree<Node> arbolBinarioBase2 = expressionQueue.Dequeue();
+                    arbolBinarioBaseStack.Push(arbolBinarioBase2);
+                    if (arbolBinarioBaseStack.Count >= 3)
+                    {
+                        BynaryTree<Node> arbolBinarioBase3 = arbolBinarioBaseStack.Pop();
+                        BynaryTree<Node> arbolBinarioBase4 = arbolBinarioBaseStack.Pop();
+                        BynaryTree<Node> arbolBinarioBase5 = arbolBinarioBaseStack.Pop();
+                        arbolBinarioBaseStack.Push(new BynaryTree<Node>(new Node("."))
+                        {
+                            Right = (IBinaryTree<Node>)arbolBinarioBase4,
+                            Left = (IBinaryTree<Node>)arbolBinarioBase5
+                        });
+                        arbolBinarioBaseStack.Push(arbolBinarioBase3);
+                    }
+                }
+                else
+                {
+                    if (!this.isOperator(arbolBinarioBase1))
+                        throw new Exception("Se encontro un simbolo no conocido");
+                    if ("+*?".Contains(arbolBinarioBase1.Value.Simbol))
+                    {
+                        BynaryTree<Node> arbolBinarioBase2 = expressionQueue.Dequeue();
+                        arbolBinarioBase2.Left = (IBinaryTree<Node>)arbolBinarioBaseStack.Pop();
+                        arbolBinarioBaseStack.Push(arbolBinarioBase2);
+                    }
+                    else if ("(".Equals(arbolBinarioBase1.Value.Simbol))
+                    {
+                        expressionQueue.Dequeue();
+                        arbolBinarioBaseStack.Push(this.generateExpression(ref expressionQueue));
+                        if (arbolBinarioBaseStack.Count >= 3)
+                        {
+                            BynaryTree<Node> arbolBinarioBase2 = arbolBinarioBaseStack.Pop();
+                            BynaryTree<Node> arbolBinarioBase3 = arbolBinarioBaseStack.Pop();
+                            BynaryTree<Node> arbolBinarioBase4 = arbolBinarioBaseStack.Pop();
+                            arbolBinarioBaseStack.Push(new BynaryTree<Node>(new Node("."))
+                            {
+                                Right = (IBinaryTree<Node>)arbolBinarioBase3,
+                                Left = (IBinaryTree<Node>)arbolBinarioBase4
+                            });
+                            arbolBinarioBaseStack.Push(arbolBinarioBase2);
+                        }
+                    }
+                    else
+                    {
+                        if (")".Equals(arbolBinarioBase1.Value.Simbol))
+                        {
+                            if (arbolBinarioBaseStack.Count <= 1)
+                                return arbolBinarioBaseStack.Pop();
+                            return new BynaryTree<Node>(new Node("."))
+                            {
+                                Right = (IBinaryTree<Node>)arbolBinarioBaseStack.Pop(),
+                                Left = (IBinaryTree<Node>)arbolBinarioBaseStack.Pop()
+                            };
+                        }
+                        if ("|".Equals(arbolBinarioBase1.Value.Simbol))
+                        {
+                            if (arbolBinarioBaseStack.Count <= 1)
+                                return arbolBinarioBaseStack.Pop();
+                            return new BynaryTree<Node>(new Node("."))
+                            {
+                                Right = (IBinaryTree<Node>)arbolBinarioBaseStack.Pop(),
+                                Left = (IBinaryTree<Node>)arbolBinarioBaseStack.Pop()
+                            };
+                        }
+                    }
+                }
+            }
+            return arbolBinarioBaseStack.Pop();
         }
 
     }
