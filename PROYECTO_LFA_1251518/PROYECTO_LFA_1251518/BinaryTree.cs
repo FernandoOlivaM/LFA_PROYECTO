7960b679439f3263c8ad3f18ab6005fd02380807
feroliv4z <feroliv4z@gmail.com>
2020-04-08 18:57:02 -0600

    agregando arbol binario tipo t

---
+++ PROYECTO_LFA_1251518/PROYECTO_LFA_1251518/BinaryTree.cs
@@ -0,0 +1,83 @@
+﻿namespace PROYECTO_LFA_1251518
+{
+    public class BynaryTree<T> : IBinaryTree<T>
+    {
+        private IBinaryTree<T> right = (IBinaryTree<T>)null;
+        private IBinaryTree<T> left = (IBinaryTree<T>)null;
+        private IBinaryTree<T> parent = (IBinaryTree<T>)null;
+        private T value;
+
+        public BynaryTree(T value) : this(value, (IBinaryTree<T>)null, (IBinaryTree<T>)null)
+        {
+        }
+
+        public BynaryTree(T value, IBinaryTree<T> left, IBinaryTree<T> right)
+        {
+            this.Value = value;
+            this.Left = left;
+            this.Right = right;
+            this.Parent = (IBinaryTree<T>)null;
+        }
+
+        public T Value
+        {
+            get
+            {
+                return this.value;
+            }
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public IBinaryTree<T> Left
+        {
+            get
+            {
+                return this.left;
+            }
+            set
+            {
+                this.left = value;
+            }
+        }
+
+        public IBinaryTree<T> Right
+        {
+            get
+            {
+                return this.right;
+            }
+            set
+            {
+                this.right = value;
+            }
+        }
+
+        public IBinaryTree<T> Parent
+        {
+            get
+            {
+                return this.parent;
+            }
+            set
+            {
+                this.parent = value;
+            }
+        }
+
+
+        public void inOrder(TraversalTree<T> node)
+        {
+            if (this.Left != null)
+                this.Left.inOrder(node);
+            node((IBinaryTree<T>)this);
+            if (this.Right == null)
+                return;
+            this.Right.inOrder(node);
+        }
+
+    }
+
+}
---
+++ PROYECTO_LFA_1251518/PROYECTO_LFA_1251518/IBinaryTree.cs
@@ -0,0 +1,12 @@
+﻿namespace PROYECTO_LFA_1251518
+{
+    public interface IBinaryTree<T>
+    {
+        T Value { get; set; }
+        IBinaryTree<T> Left { get; set; }
+        IBinaryTree<T> Right { get; set; }
+        IBinaryTree<T> Parent { get; set; }
+        void inOrder(TraversalTree<T> visitar);
+    }
+
+}
